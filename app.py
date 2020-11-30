a427253edce66d5439f765c7888f78caf6209241
fujute <fuju@narisa.com>
2020-11-30 17:09:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+import dateime
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    now = datetime.datetime.now()
+    print ("Azure TechKnight 26-Nov-2020", now)
+    myText = "Hello, Azure " + now.strftime("%X")
+    return myText
